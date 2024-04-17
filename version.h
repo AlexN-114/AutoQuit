ba40f5bbfa1e811a52a8c30784723e710858203b
Alexander Neumeister <alexander.neumeister@siemens.com>
2024-04-17 07:32:15 +0200

    Version nachgezogen

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 
 	//Date Version Types
-	static const char DATE[] = "15";
+	static const char DATE[] = "16";
 	static const char MONTH[] = "04";
 	static const char YEAR[] = "2024";
 	static const char UBUNTU_VERSION_STYLE[] =  "24.03";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 0;
-	static const long BUILD  = 4;
-	static const long REVISION  = 7;
+	static const long BUILD  = 5;
+	static const long REVISION  = 9;
 
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT  = 36;
-	#define RC_FILEVERSION 1,0,4,7
-	#define RC_FILEVERSION_STRING "1, 0, 4, 7\0"
-	static const char FULLVERSION_STRING [] = "1.0.4.7";
+	#define RC_FILEVERSION 1,0,5,9
+	#define RC_FILEVERSION_STRING "1, 0, 5, 9\0"
+	static const char FULLVERSION_STRING [] = "1.0.5.9";
 
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 4;
+	static const long BUILD_HISTORY  = 5;
 
 }
 #endif //VERSION_H
